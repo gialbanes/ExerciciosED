f2703459edb5e8699607ac8349616e25fcb7864e
gialbanes <albanesgiovana@gmail.com>
2024-09-26 20:37:10 -0300

    aula 19-09 add modificações

--- ponteiros/aula19-09/exercicio1/main.c
+++ ponteiros/aula19-09/exercicio1/main.c
@@ -7,14 +7,12 @@ int main()
     int *p=vet;
 
     for(int i=0; i<100; i++){
-        *p=0;
-        p++;
+        *(p+i) = 0;
     }
 
     p=vet;
     for(int i=1; i<=100; i++){
-        *p=i;
-        p++;
+        *(p + i) = i + 1;
     }
 
     p=vet;
